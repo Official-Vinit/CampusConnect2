bf7feadf35e590259c54d77825117553d0c5934c
Official-Vinit <officialvinit1176@gmail.com>
2025-04-18 22:17:20 +0530

    mongo changes

--- index.js
+++ index.js
@@ -26,7 +26,14 @@ main()
 });
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/campusconnect2");
+    await mongoose.connect("mongodb://127.0.0.1:27017/campusconnect2", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log("Connected to MongoDB");
+    }).catch(err => {
+        console.error("Error connecting to MongoDB:", err);
+    });
 }
 
 //home route
